77372eefca1fdc5e77067dd0dcf7b07369b83c15
Tara Glennie <gleta063@student.otago.ac.nz>
2023-04-06 22:56:06 +1200

    Fixed error appearing on server load

--- socialAlumni/routes/index.js
+++ socialAlumni/routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Social Alumni' });
 });
+
+router.get('/signIn', function(req, res) {
+  res.render('sign-in', {title: 'Sign In' });
+});
+
 
 
 module.exports = router;
