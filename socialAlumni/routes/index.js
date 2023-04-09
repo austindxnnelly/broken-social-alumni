a77f404adc74fa544085aa4c009a637de035266c
Nicholas <yunni332@student.otago.ac.nz>
2023-04-09 18:53:10 +1200

    Added more to navigation menu

--- socialAlumni/routes/index.js
+++ socialAlumni/routes/index.js
@@ -15,5 +15,10 @@ router.get('/create', (req, res) => {
   res.render('create', {title: "Create Account"});
 });
 
+router.get('/nav', (req, res) => {
+  const isAuthenticated = req.isAuthenticated();
+  res.render('nav', { isAuthenticated });
+});
+
 
 module.exports = router;
