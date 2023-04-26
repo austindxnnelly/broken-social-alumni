4a5c9ef0eb046c766ea3882ed30c81ecbc4223b0
vanma236 <vanma236@student.otago.ac.nz>
2023-04-26 20:06:21 +1200

    Added a sanity test to make sure the site starts properly.

---
+++ socialAlumni/test/test.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('Sanity check', function() {
+    it('Should launch the application without errors', function() {
+      require('../app.js'); 
+    });
+  });
+  
