fcbbd5c9e0e6cd44a4eecdc8396404a8e906ada8
vanma236 <vanma236@student.otago.ac.nz>
2023-05-09 18:35:40 +1200

    Testing if the test can fail

--- socialAlumni/test/test.js
+++ socialAlumni/test/test.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 //Test to make sure continous integration checking works
 test('My Test', (t) => {
-  t.equal(1 + 1, 2, '1+1 should equal 2');
+  t.equal(1 + 0, 2, '1+1 should equal 2');
   t.end();
 });
